cbd9918b30ac5ba2338332603d1366b240d8efc9
tushar-asodariya <tusharasodariya1@gmail.com>
2022-02-23 02:31:24 +0530

    initial commit

--- .idea/lib/core/constants/constants.dart
+++
@@ -1,1 +0,0 @@
-String currencySymbol = 'S\$';
--- .idea/lib/presentation/wallets_screen/models/transaction_model.dart
+++
@@ -1,9 +0,0 @@
-class TransactionData{
-  DateTime? dataTime;
-  String? title;
-  String? subTitle;
-  String? amount;
-  String? totalAmt;
-  String? image;
-  TransactionData({this.image,this.amount,this.dataTime,this.subTitle,this.title,this.totalAmt});
-}
